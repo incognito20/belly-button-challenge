c9e3f18c9584b004a54f0739943e17572a04d965
incognito20 <chadjrichards@gmail.com>
2023-05-30 20:19:10 -0500

    Updates to Part 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,46 @@
-// set the url to variable
+//Set url variable
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-var sampleData;
-//Fetch the JSON data and console log it
+// Fetch the json data
 d3.json(url).then(function(data) {
-  sampleData = data.sample_values;
   console.log(data);
-   });
 
-let sortedSampleData = sampleData.sort((a, b) => b.sample_values - a.sample_values);
-let slicedSampleData = sortedSampleData.slice(0, 10);
-let reversedSampleData = slicedSampleData.reverse();
 
-let trace = {
-        x: reversedSampleData.map(object => object.sample_values),
-        y: reversedSampleData.map(object => object.otu_ids),
-        text: reversedSampleData.map(object => object.otu_ids),
-        type: "bar",
-        orientation: "h",
-        marker: {
-            color: "blue"
-        }
-    };
+  // Sort the data 
+  let sortData = data.sort((a, b) => b.values - a.values);
 
-    let layout = {
-        height: 400,
-        width: 500,
-        margin: {
-            t: 10
-        }
-    };
+  //Slice data for top 10
+  let sliceData = sortData.slice(0, 10);
 
-    let traceData = [trace];
-    
-    Plotly.newPlot("bar", traceData, layout);
+  //Reverse order
+  let reverseData = sliceData.reverse();
+
+
+  // Create trace for the horizontal bar chart
+  let trace = {
+    x: reverseData.map(Object => Object.values),
+    y: reverseData.map(Object => `OTU ${Object.otu_ids}`),
+    text: reversedData.map(Object => Object.otu_labels),
+    name: "Top 10 OTU's",
+    type: "bar",
+    orientation: "h"
+  };
+
+  // Data for the chart
+  let traceData = [trace];
+
+  // Layout for the chart
+  let layout = {
+    title: "Top 10 OTUs",
+    margin: {
+      l: 125,
+      r: 125,
+      t: 125,
+      b: 125
+    }
+  };
+
+  // Draw chart
+  Plotly.newPlot("bar", traceData, layout);
+
+  });
