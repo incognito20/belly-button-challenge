04a623ee5245b2f8b10c31d7c326a59587e60a8a
incognito20 <chadjrichards@gmail.com>
2023-06-04 17:39:36 -0500

    Updates to chart code

--- static/js/app.js
+++ static/js/app.js
@@ -1,46 +1,109 @@
-//Set url variable
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+// // Set variable to URL
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Fetch the json data
-d3.json(url).then(function(data) {
-  console.log(data);
+// Set variables for samples and metadata
+var samples;
+var mData;
 
+//Set JSON to data and assign values to variables and call each function for each id
+d3.json(url).then(function (data) {
+    let select = d3.select("#selDataset");
+    samples = data.samples;
+    mdata = data.metadata;
+    data.names.forEach((id) => {select.append("option").text(id).property("value", id);
+    });
+    metaData(mdata[0]);
+    horizBarChart(samples[0]);
+    bubbleChart(samples[0]);
+});
 
-  // Sort the data 
-  let sortData = data.sort((a, b) => b.values - a.values);
+//When option is changed select new id and call each function again
+function optionChanged(value) {
+    var selectId = samples.find((item) => item.id === value);
+    var demoInfo = mdata.find((item) => item.id == value);
 
-  //Slice data for top 10
-  let sliceData = sortData.slice(0, 10);
+// Calls for demographic data
+    metaData(demoInfo);
 
-  //Reverse order
-  let reverseData = sliceData.reverse();
+// Calls for bar chart
+    horizBarChart(selectId);
 
+// Calls for bubble Chart
+    bubbleChart(selectId);
 
-  // Create trace for the horizontal bar chart
-  let trace = {
-    x: reverseData.map(Object => Object.values),
-    y: reverseData.map(Object => `OTU ${Object.otu_ids}`),
-    text: reversedData.map(Object => Object.otu_labels),
-    name: "Top 10 OTU's",
-    type: "bar",
-    orientation: "h"
-  };
+}
+//Pulls each of the demographic items and sets each title
+function metaData(demoInfo) {
+    let demoSelect = d3.select("#sample-metadata");
 
-  // Data for the chart
-  let traceData = [trace];
+    demoSelect.html(
+    `ID: ${demoInfo.id} <br> 
+    Ethnicity: ${demoInfo.ethnicity} <br>
+    Gender: ${demoInfo.gender} <br>
+    Age: ${demoInfo.age} <br>
+    Location: ${demoInfo.location} <br>
+    BBType: ${demoInfo.bbtype} <br>
+    WFreq: ${demoInfo.wfreq}`
+    );
+}
+//HBar chart. Reverses order and charts sample values and otu_ids for top 10
+function horizBarChart(selectId) {
+    let x = selectId.sample_values.slice(0, 10).reverse();
+    let y = selectId.otu_ids
+        .slice(0, 10)
+        .reverse()
+        .map((item) => `OTU ${item}`);
+    let text = selectId.otu_labels.slice(0, 10).reverse();
 
-  // Layout for the chart
-  let layout = {
-    title: "Top 10 OTUs",
-    margin: {
-      l: 125,
-      r: 125,
-      t: 125,
-      b: 125
-    }
-  };
+    barChart = {
+        x: x,
+        y: y,
+        text: text,
+        type: "bar",
+        orientation: "h",
+    };
 
-  // Draw chart
-  Plotly.newPlot("bar", traceData, layout);
+    let chart = [barChart];
 
-  });
+    let layout = {
+        margin: {
+            l: 100,
+            r: 100,
+            t: 0,
+            b: 100,
+        },
+        height: 500,
+        width: 600,
+    };
+
+    Plotly.newPlot("bar", chart, layout);
+}
+//Bubble chart for otu ids and sample values.
+function bubbleChart(selectId) {
+    let x = selectId.otu_ids;
+    let y = selectId.sample_values;
+    let marker_size = selectId.sample_values;
+    let color = selectId.otu_ids;
+    let text = selectId.otu_labels;
+
+    bubble = {
+        x: x,
+        y: y,
+        text: text,
+        mode: "markers",
+        marker: {
+            color: color,
+            colorscale: "Picnic",
+            size: marker_size,
+        },
+        type: "scatter",
+    };
+    let chart = [bubble];
+
+    let layout = {
+        xaxis: {
+            title: { text: "OTU ID" },
+        },
+    };
+    Plotly.newPlot("bubble", chart, layout);
+}
